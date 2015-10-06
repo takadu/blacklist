e2a8984312f50eb55e429ac9da3bb6f8b1089675
Adi Baron <adi.baron@takadu.com>
2015-10-06 17:45:04 +0300

    fix bintray credentials

--- .travis.credentials.sh
+++ .travis.credentials.sh
@@ -6,9 +6,6 @@ FILE=$HOME/.bintray/.credentials
 cat <<EOF >$FILE
 realm = Bintray API Realm
 host = api.bintray.com
-user = $BINTRAY_USER
-password = $BINTRAY_API_KEY
+user = takadu
+password = 4e600fc2274a7c10a5a53c6a22fdf0ad46cd7dbe
 EOF
-
-echo $BINTRAY_USER
-echo $BINTRAY_API_KEY
